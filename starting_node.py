e8d1d9788d4a9bf7c7085831ff3fdb6ee90492db
madsysharma <108534984+madsysharma@users.noreply.github.com>
2024-06-10 11:13:53 -0700

    Update starting_node.py - fixed format check logic

--- starting_node.py
+++ starting_node.py
@@ -10,9 +10,10 @@ if not os.path.exists(directory_path):
 self_ip = sys.argv[1]
 regexp_ip=re.compile("^10\.16\.9\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
 regexp_name=re.compile("linux224\\d{2}")
-if len(re.findall(regexp_ip,self_ip))==0 or len(re.findall(regexp_name,self_ip))==0:
-    print("Invalid format of IP.")
-    sys.exit(0)
+if len(re.findall(regexp_ip,self_ip))>0 or len(re.findall(regexp_name,self_ip))>0:
+    print("Valid IPs/hostnames.")
+else:
+    print("Invalid IPs/hostnames.")
 
 chordServer = ChordServer(self_ip)
 chordServer.start_server()
