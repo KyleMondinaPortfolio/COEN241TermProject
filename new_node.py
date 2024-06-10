9e1dd0061dad1e038441384332ed15c9ba0f0bc0
madsysharma <108534984+madsysharma@users.noreply.github.com>
2024-06-10 11:15:35 -0700

    Update new_node.py - fixed IP/hostname format check logic

--- new_node.py
+++ new_node.py
@@ -15,8 +15,10 @@ bootstrap_ip = sys.argv[2]
 regexp_ip=re.compile("^10\.16\.9\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
 regexp_name=re.compile("linux224\\d{2}")
 
-if len(re.findall(regexp_ip,self_ip))==0 or len(re.findall(regexp_name,self_ip))==0 len(re.findall(regexp_ip,bootstrap_ip))==0 or len(re.findall(regexp_name,bootstrap_ip))==0:
-    print("Invalid IP format.")
+if len(re.findall(regexp_ip,self_ip))>0 or len(re.findall(regexp_name,self_ip))>0 or len(re.findall(regexp_ip,bootstrap_ip))>0 or len(re.findall(regexp_name,bootstrap_ip))>0:
+    print("Valid IPs/hostnames.")
+else:
+    print("Invalid IPs/hostnames.")
     sys.exit(0)
 
 chordServer = ChordServer(self_ip,bootstrap_ip)
